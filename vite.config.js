0cbf293843da4d8c7f4675a5d227ffaaf4e1d16e
moataz Alaa <moattazalaa@gmail.com>
2024-09-09 14:51:35 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/E-commerce/"
-  
+  base: "/E-commerce/",
+  build: {
+    chunkSizeWarningLimit: 1600
+  }
+
 })
